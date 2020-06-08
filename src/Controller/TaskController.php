17fc3cf38fa27c28a81d01911d9f079f6a516229
Vincent BRAY <vincentbray@free.fr>
2020-06-08 14:41:42 +0200

    Editer la fonction taskListing()

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Task;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,8 +11,18 @@ class TaskController extends AbstractController
     /**
      * @Route("/tasks/listing", name="tasks_listing")
      */
-    public function index()
+    public function taskListing()
     {
+
+        // On va chercher par doctrine le repository de nos Task
+        $repository = $this->getDoctrine()->getRepository(Task::class);
+
+        // dans ce repository nous récupérons toutes les données
+        $tasks = $repository->findAll();
+
+        // affichage des données dans var_dumper
+        dd($tasks);
+
         return $this->render('task/index.html.twig', [
             'controller_name' => 'TaskController',
         ]);
