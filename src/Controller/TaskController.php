391475aa53b5d8c09dc24eafa68d968fce62a2c0
Vincent BRAY <vincentbray@free.fr>
2020-06-09 14:27:12 +0200

    Création du formulaire de création de tâche

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Task;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\TaskType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class TaskController extends AbstractController
 {
@@ -26,5 +29,23 @@ class TaskController extends AbstractController
         return $this->render('task/index.html.twig', [
             'tasks' => $tasks,
         ]);
+    }
+
+    /**
+     * @Route("/tasks/create", name="task_create")
+     * @param Request $request
+     * @return Response
+     */
+    public function createTask(Request $request): Response
+    {
+        // On crée un objet Task
+        $task = new Task();
+
+        $task->setCreatedAt(new \DateTime);
+
+        // On crée notre formulaire
+        $form = $this->createForm(TaskType::class, $task, array());
+
+        return $this->render('task/create.html.twig', ['form' => $form->createView()]);
     }
 }
---
+++ src/Form/TaskType.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Tag;
+use App\Entity\Task;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class TaskType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class, array('label' => 'Nom de la tâche', 'attr' => array(
+                'class' => 'form-control',
+                'title' => 'Nom de la tâche',
+            )))
+            ->add('description', TextareaType::class, array('label' => 'Description', 'attr' => array(
+                'class' => 'form-control',
+                'title' => 'Description',
+            )))
+            ->add('dueAt', DateTimeType::class, array('widget' => 'single_text', 'label' => 'Date effective', 'attr' => array(
+                'class' => 'form-control',
+                'title' => 'Date effective',
+            )))
+            ->add('tag', EntityType::class, [
+                'class' => Tag::class,
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('c')->orderBy('c.name', 'ASC');
+                },
+                'choice_label' => 'name',
+                'label' => 'Catégorie',
+                'attr' => array(
+                    'class' => 'form-control',
+                    'title' => 'Catégorie',
+                )
+            ])
+            ->add('save', SubmitType::class, array(
+                'label' => 'Enregistrer',
+                'attr' => array(
+                    'class' => 'btn btn-primary',
+                    'title' => 'Enregistrer'
+                )
+            ));
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Task::class,
+        ]);
+    }
+}
