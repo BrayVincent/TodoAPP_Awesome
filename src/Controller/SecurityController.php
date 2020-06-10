0165f47cd0a07de75d53b6db004d58980fc7f926
Vincent BRAY <vincentbray@free.fr>
2020-06-10 11:08:02 +0200

    Sécurisation de nos routes

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -14,9 +14,10 @@ class SecurityController extends AbstractController
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+        // Si l'utilisateur est déjà connecté, le rediriger vers la page de listing
+        if ($this->getUser()) {
+            return $this->redirectToRoute('tasks_listing');
+        }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
@@ -31,6 +32,5 @@ class SecurityController extends AbstractController
      */
     public function logout()
     {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
