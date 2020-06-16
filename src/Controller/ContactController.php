ac6c279dc252e74c43b37dc7a55d55df2411d961
Vincent BRAY <vincentbray@free.fr>
2020-06-16 14:21:41 +0200

    envoi mail v2

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,13 +4,14 @@ namespace App\Controller;
 
 use App\Entity\Task;
 use App\Form\TaskType;
+use App\Form\TaskMailType;
 use App\Repository\TaskRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Contracts\Translation\TranslatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ContactController extends AbstractController
 {
@@ -50,25 +51,45 @@ class ContactController extends AbstractController
      * @param \Swift_Mailer $mailer
      * @return Response
      */
-    public function sendTask(\Swift_Mailer $mailer): Response
+    public function sendTask(Task $task = null, \Swift_Mailer $mailer, Request $request): Response
     {
-        //récupérer les infos de l'utilisateur
+
+        //récupérer les infos de l'utilisateur 
         $user = $this->getUser();
-        //Récupère la tâche par son id
+
+        //on récupère l'adresse mail de l'utilisateur 
+        $usermail = $user->getEmail();
+
+        //on crée notre formulaire 
+        $form = $this->createForm(TaskMailType::class, array('email' => $usermail));
+
+        // On récupère notre formulaire 
+        $form->handleRequest($request);
+
+        //Récupère la tâche par son id 
         $id = $_GET['id'];
+
         $task = $this->repository->findOneById($id);
 
-        //Envoi du mail
-        $message = (new \Swift_Message('Voici votre tâche'))
-            ->setFrom($_SERVER['MAILER_ADDRESS'])
-            ->setTo($user->getEmail())
-            ->setBody($this->renderView('mail/task.html.twig', ['task' => $task]), 'text/html');
-        $mailer->send($message);
-        $this->addFlash(
-            'success',
-            $this->translator->trans('task.mail.success')
-        );
+        //on vérifie si le formulaire est soumi 
+        if ($form->isSubmitted() and $form->isValid()) {
 
-        return $this->redirectToRoute('tasks_listing');
+            //on récupère la valeur de notre input email 
+            $adress = $form->get('email')->getData();
+
+            //Envoi du mail 
+            $message = (new \Swift_Message('Voici votre tâche'))
+                ->setFrom($_SERVER['MAILER_ADDRESS'])
+                ->setTo($adress)
+                ->setBody($this->renderView('mail/task.html.twig', ['task' => $task]), 'text/html');
+
+            $mailer->send($message);
+
+            $this->addFlash('success', 'Vous avez bien envoyé votre tâche !');
+
+            return $this->redirectToRoute('tasks_listing');
+        }
+        // return $this->redirectToRoute('tasks_listing'); 
+        return $this->render('mail/formSendTo.html.twig', ['task' => $task, 'form' => $form->createView()]);
     }
 }
---
+++ src/Form/TaskMailType.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class TaskMailType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('email', EmailType::class, array('label' => "Veuillez saisir l'adresse de destination :", 'attr' => array('class' => 'form-control', 'title' => 'Destinataire')))->add('save', SubmitType::class, array('label' => 'Envoyer', 'attr' => array('class' => 'btn btn-outline-primary', 'title' => 'Envoyer')));
+    }
+}
